78f517f0be9e24c816c8268f1f10f3d152e969e4
Mayowa-jerrie <108583250+Mayowa-jerrie@users.noreply.github.com>
2023-03-04 13:40:30 +0100

    reformed javascript

--- app.js
+++ app.js
@@ -12,3 +12,89 @@ sub.addEventListener("click", () =>{
 resetCount.addEventListener("click", () =>{
     count.innerHTML = 0 ;
 })
+
+
+
+const count = document.querySelector(".count")
+const add = document.querySelector(".add")
+const resetCount = document.querySelector(".reset")
+const sub = document.querySelector(".subtract")
+
+add.addEventListener("click", () =>{
+    count.innerHTML++;
+})
+sub.addEventListener("click", () =>{
+    count.innerHTML--;
+})
+resetCount.addEventListener("click", () =>{
+    count.innerHTML = 0 ;
+})
+
+
+const count = document.querySelector(".count")
+const add = document.querySelector(".add")
+const resetCount = document.querySelector(".reset")
+const sub = document.querySelector(".subtract")
+
+add.addEventListener("click", () =>{
+    count.innerHTML++;
+})
+sub.addEventListener("click", () =>{
+    count.innerHTML--;
+})
+resetCount.addEventListener("click", () =>{
+    count.innerHTML = 0 ;
+})
+
+
+
+
+const count = document.querySelector(".count")
+const add = document.querySelector(".add")
+const resetCount = document.querySelector(".reset")
+const sub = document.querySelector(".subtract")
+
+add.addEventListener("click", () =>{
+    count.innerHTML++;
+})
+sub.addEventListener("click", () =>{
+    count.innerHTML--;
+})
+resetCount.addEventListener("click", () =>{
+    count.innerHTML = 0 ;
+})
+
+
+
+const count = document.querySelector(".count")
+const add = document.querySelector(".add")
+const resetCount = document.querySelector(".reset")
+const sub = document.querySelector(".subtract")
+
+add.addEventListener("click", () =>{
+    count.innerHTML++;
+})
+sub.addEventListener("click", () =>{
+    count.innerHTML--;
+})
+resetCount.addEventListener("click", () =>{
+    count.innerHTML = 0 ;
+})
+
+
+
+
+const count = document.querySelector(".count")
+const add = document.querySelector(".add")
+const resetCount = document.querySelector(".reset")
+const sub = document.querySelector(".subtract")
+
+add.addEventListener("click", () =>{
+    count.innerHTML++;
+})
+sub.addEventListener("click", () =>{
+    count.innerHTML--;
+})
+resetCount.addEventListener("click", () =>{
+    count.innerHTML = 0 ;
+})
